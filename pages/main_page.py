ceed36aa005fafa38c5a96316a30c8177e844af8
Artur Przybyło <a.przybyloo@gmail.com>
2022-09-11 18:31:32 +0200

    Added screenshot on tests failing

--- pages/main_page.py
+++ pages/main_page.py
@@ -10,9 +10,10 @@ class MainPage(BasePage):
     _sign_in_button = (By.XPATH, "//a[@class='login']")
     _sign_out_button = (By.XPATH, "//a[@class='logout']")
 
-    def search(self, search):
+    def search(self, search_expression):
         search_bar = SearchBarRegion(self.driver)
-        search_bar.search(search)
+        search_bar.search(search_expression)
+        self.ca.wait_for_visibility(self._sign_in_button, click=True)
 
     def click_login_button(self):
         self.ca.wait_for_visibility(self._sign_in_button, click=True)
